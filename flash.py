04278f2528bec9f0fceae659cfa44122506bc411
alexlynd <22926680+alexlynd@users.noreply.github.com>
2022-08-15 18:04:02 -0700

    update flasher tool

---
+++ flash.py
@@ -0,0 +1,16 @@
+import os, subprocess
+import serial.tools.list_ports
+
+ports = serial.tools.list_ports.comports()
+portsInUse = []
+
+
+for port, desc, hwid in sorted(ports):
+	
+	if (desc == "ESP32-S2" and (port not in portsInUse) ):
+		portsInUse.append(port)
+		print("------------------------------")
+		print("Found ESP32-S2!")
+		print("Erasing and flashing board...")
+		os.system('gnome-terminal --working-directory ~ -e \"esptool.py --port {} --chip esp32s2 write_flash 0x0 v1.1rubber_nugget.bin\"'.format(port))
+		
