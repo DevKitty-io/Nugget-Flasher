db2df2eaf1f530249904654495e0077c89e1d305
Alex Lynd <22926680+AlexLynd@users.noreply.github.com>
2022-10-28 02:32:56 -0700

    wait for processes to terminate

--- esp.py
+++ esp.py
@@ -5,13 +5,16 @@ import esptool
 import serial.tools.list_ports
 ports = serial.tools.list_ports.comports()
 
+processes = []
+
 for port, desc, hwid in sorted(ports):
         
         if ("303A:0002" in hwid):
                 print("ESP32-S2 found on port: {}".format(port))
                 proc = subprocess.Popen(["esptool.py --port {} --after no_reset write_flash 0x0 tester.bin".format(port)],shell=True)
-                
-                # esptool.main(flash_cmd)
+                processes.append(proc)
+for p in processes:
+        p.wait()
 print("###################################")
 print("FINISHED FLASHING")
 print("###################################")
